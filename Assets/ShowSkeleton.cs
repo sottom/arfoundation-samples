6d9d83fc74e95ef74aa960e6f6ba577464b2234d
Mitchell <sottochoro@gmail.com>
2020-03-04 12:36:38 -0700

    change show body script

--- Assets/ShowSkeleton.cs
+++ Assets/ShowSkeleton.cs
@@ -4,11 +4,10 @@ using UnityEngine.XR.ARFoundation;
 public class ShowSkeleton : MonoBehaviour
 {
     // don't need to do both of these
-    public HumanBodyTracker humanBodyTracker;
-    public ARHumanBodyManager humanBodyManager;
+    public GameObject humanBodyTracking;
 
     public void ShowBody(){
-        humanBodyManager.humanBodyPose3DEstimationEnabled = true;
-        humanBodyTracker.enabled = true;
+        // humanBodyManager.humanBodyPose3DEstimationEnabled = true;
+        humanBodyTracking.SetActive(true);
     }
 }
