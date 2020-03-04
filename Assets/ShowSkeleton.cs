6b40b2951de30358de8a4ce1bf744ee80c89d5fa
Mitchell <sottochoro@gmail.com>
2020-03-04 12:02:07 -0700

    testing button

---
+++ Assets/ShowSkeleton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ShowSkeleton : MonoBehaviour
+{
+    // don't need to do both of these
+    public HumanBodyTracker humanBodyTracker;
+    public ARHumanBodyManager humanBodyManager;
+
+    public void ShowBody(){
+        humanBodyManager.humanBodyPose3DEstimationEnabled = true;
+        humanBodyTracker.enabled = true;
+    }
+}
